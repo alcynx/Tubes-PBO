99b0dff1968a2972f0000f3a8a6c00b46c1919be
desssti06 <11221033@student.itk.ac.id>
2023-06-08 00:59:47 +0800

    tambah method tambah, hapus, edit data

--- data_kas.java
+++ data_kas.java
@@ -1,49 +1,87 @@
 import java.util.Date;
 
 public class data_kas {
-   private float Pengeluaran;
-   private float Pemasukan;
-   private int NoTransaksi;
-   public Date TglTransaksi;
-   private float Saldo;
-   private String Keterangan;
-   
-   public data_kas(float Pengeluaran, float Pemasukan,int NoTransaksi,Date TglTransaksi, float Saldo, String Keterangan){
-   this.Pengeluaran = Pengeluaran;
-   this.Pemasukan = Pemasukan;
-   this.NoTransaksi = NoTransaksi;
-   this.Saldo = Saldo;
-   this.Keterangan = Keterangan;
-   }
-   public float getPengeluaran(){
-       return this.Pengeluaran;
-   }
-   public void setPengeluaran(float Pengeluaran){
-       this.Pengeluaran = Pengeluaran;
-   }
-   public float getPemasukan(){
-       return this.Pemasukan;
-   }
-   public void setPemasukan(float Pemasukan){
-       this.Pemasukan = Pemasukan;
-   }
-   public int getNoTransaksi(){
-       return this.NoTransaksi;
-   }
-   public void setNoTransaksi(int NoTransaksi){
-       this.NoTransaksi = NoTransaksi;
-   }
-   public float getSaldo(){
-       return this.Saldo;
-   }
-   public void setSaldo(float Saldo){
-       this.Saldo = Saldo;
-   }
-   public String getKeterangan(){
-       return this.Keterangan;
-   }
-   public void setKeterangan(String Keterangan){
-       this.Keterangan = Keterangan;
-   }
+    private float Pengeluaran;
+    private float Pemasukan;
+    private int NoTransaksi;
+    public Date TglTransaksi;
+    private float Saldo;
+    private String Keterangan;
+    
+    // Constructors
+    public data_kas(float Pengeluaran, float Pemasukan,int NoTransaksi,Date TglTransaksi, float Saldo, String Keterangan){
+        this.Pengeluaran = Pengeluaran;
+        this.Pemasukan = Pemasukan;
+        this.NoTransaksi = NoTransaksi;
+        this.Saldo = Saldo;
+        this.Keterangan = Keterangan;
+    }
+
+    // Method Setter Getter
+    public float getPengeluaran(){
+        return this.Pengeluaran;
+    }
+    public void setPengeluaran(float Pengeluaran){
+        this.Pengeluaran = Pengeluaran;
+    }
+    public float getPemasukan(){
+        return this.Pemasukan;
+    }
+    public void setPemasukan(float Pemasukan){
+        this.Pemasukan = Pemasukan;
+    }
+    public int getNoTransaksi(){
+        return this.NoTransaksi;
+    }
+    public void setNoTransaksi(int NoTransaksi){
+        this.NoTransaksi = NoTransaksi;
+    }
+    public float getSaldo(){
+        return this.Saldo;
+    }
+    public void setSaldo(float Saldo){
+        this.Saldo = Saldo;
+    }
+    public String getKeterangan(){
+        return this.Keterangan;
+    }
+    public void setKeterangan(String Keterangan){
+        this.Keterangan = Keterangan;
+    }
+
+   // Method Tambah
+    public void TambahKas (float Pengeluaran, float Pemasukan,int NoTransaksi,Date TglTransaksi, float Saldo, String Keterangan){
+        this.Pengeluaran = Pengeluaran;
+        this.Pemasukan = Pemasukan;
+        this.NoTransaksi = NoTransaksi;
+        this.TglTransaksi = TglTransaksi;
+        this.Saldo = Saldo;
+        this.Keterangan = Keterangan;
+        System.out.println("Kas berhasil ditambahkan!");
+    }
+
+   // method Hapus
+    public void HapusKas() {
+        this.Pengeluaran = 0;
+        this.Pemasukan = 0;
+        this.NoTransaksi = 0;
+        this.TglTransaksi = null;
+        this.Saldo = 0;
+        this.Keterangan = "";
+        System.out.println("Kas berhasil dihapus!");
+    }
+
+    // method edit
+    public void editData(float Pengeluaran, float Pemasukan,int NoTransaksi,Date TglTransaksi, float Saldo, String Keterangan){
+        this.Pengeluaran = Pengeluaran;
+        this.Pemasukan = Pemasukan;
+        this.NoTransaksi = NoTransaksi;
+        this.TglTransaksi = TglTransaksi;
+        this.Saldo = Saldo;
+        this.Keterangan = Keterangan; 
+        System.out.println("Kas berhasil diperbarui!");
+    }
+
+
 
 }
--- data_mhs.java
+++ data_mhs.java
@@ -7,11 +7,14 @@ public class data_mhs {
     private String Tlp;
     public String Gender;
     
+    // Constructors
     public data_mhs(String NIM, String Nama, String Alamat, Date Born, String Tlp, String Gender){
     this.Alamat = Alamat;
     this.Born = Born;
     this.Tlp = Tlp;
     }
+
+    // Method Setter Getter
     public String getAlamat(){
     return this.Alamat;
     }
@@ -25,10 +28,43 @@ public class data_mhs {
     this.Born = Born;
     }
 
+    // Method Tambah
+    public void tambahData(String NIM, String Nama, String Alamat, Date Born, String Tlp, String Gender) {
+    this.NIM = NIM;
+    this.Nama = Nama;
+    this.Alamat = Alamat;
+    this.Born = Born;
+    this.Tlp = Tlp;
+    this.Gender = Gender;
+    System.out.println("Data berhasil ditambahkan!");
+    }
+
+    // Method Hapus
+    public void hapusData() {
+    this.NIM = "";
+    this.Nama = "";
+    this.Alamat = "";
+    this.Born = null;
+    this.Tlp = "";
+    this.Gender = "";
+    System.out.println("Data berhasil dihapus!");
+    }
+
+    // Method Edit
+    public void editData(String NIM, String Nama, String Alamat, Date Born, String Tlp, String Gender) {
+    this.NIM = NIM;
+    this.Nama = Nama;
+    this.Alamat = Alamat;
+    this.Born = Born;
+    this.Tlp = Tlp;
+    this.Gender = Gender;
+    System.out.println("Data berhasil diperbarui!");
+    }
+
     @Override
     public String toString(){
-    return "Nama : " + Nama + 
-            "NIM : " + NIM +
+    return "Nama : " + this.Nama + 
+            "NIM : " + this.NIM +
             "Gender : " + Gender +
             "Tanggal Lahir : " + this.Born +
             "Telepon : " + this.Tlp +
