a2cfac5802862b76aeee70389646f8b845a0d1bb
Desti Nur Irawati <129352971+desssti06@users.noreply.github.com>
2023-06-19 00:45:28 +0800

    Update lp_keluar.java

--- code/lp_keluar.java
+++ code/lp_keluar.java
@@ -1,3 +1,27 @@
-public class lp_keluar {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class lp_keluar extends Kas {
+    private final List<String> daftarPengeluaran;
+
+    public lp_keluar(double saldo) {
+        super(0, saldo, 0, null, saldo, "");
+        daftarPengeluaran = new ArrayList<>();
+    }
+
+    public void tambahPengeluaran(String keterangan, double jumlah) {
+        Pengeluaran += jumlah;
+        Saldo -= jumlah;
+        daftarPengeluaran.add( keterangan + " - Rp." + jumlah);
+        System.out.println("Pengeluaran berhasil ditambahkan!");
+         
+    }
+
+    public void tampilkanLaporan() {
+        System.out.println("Laporan Pengeluaran:");
+        for (String pengeluaran : daftarPengeluaran) {
+            System.out.println(pengeluaran);
+        }
+        System.out.println("Saldo: Rp." + Saldo);
+    }
 }
