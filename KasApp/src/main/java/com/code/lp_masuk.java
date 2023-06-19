bd054fa512a40780b7b918fa75fe55aa52504cdc
Ayiais <132994229+Ayiais@users.noreply.github.com>
2023-06-19 20:48:46 +0800

    Update2 lp_masuk.java

--- KasApp/src/main/java/com/code/lp_masuk.java
+++ KasApp/src/main/java/com/code/lp_masuk.java
@@ -3,15 +3,10 @@ import java.util.List;
 
 public class Lp_masuk extends Kas{
     private final List<String>daftarPemasukan;
-    // protected double Pemasukan;
-    // protected double Saldo;
 
     public Lp_masuk(double Saldo){
         super(0, Saldo, 0, null, Saldo, "");
         daftarPemasukan = new ArrayList<>();
-        // super(Pemasukan,Pengeluaran,NoTransaksi,TglTransaksi,Keterangan,Saldo);
-        // this.Pemasukan = Pemasukan;
-        // this.Saldo = Saldo;
     }
 
     public void tambahPemasukan(String keterangan, double jumlah){
@@ -28,17 +23,4 @@ public class Lp_masuk extends Kas{
         }
         System.out.println("Saldo : Rp. " + Saldo);
     }
-
-    // public double getPemasukan(){
-    //     return this.Pemasukan;
-    // }
-    // public void setPemasukan(Double Pemasukan){
-    //     this.Pemasukan = Pemasukan;
-    // }
-    // public double getSaldo(){
-    //     return this.Saldo;
-    // }
-    // public void setSaldo(Double Saldo){
-    //     this.Saldo = Saldo;
-    // }
 }
